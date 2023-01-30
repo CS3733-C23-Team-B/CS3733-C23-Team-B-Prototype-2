5192ec632f4f476d065e8edd2af79e501da0b18d
Jwalts23 <81712807+Jwalts23@users.noreply.github.com>
2023-01-30 15:05:30 -0500

    filled in tehe rest of the objects and made class good for singleton use

--- src/main/java/edu/wpi/teamb/Entities/patientTransportationRequest.java
+++ src/main/java/edu/wpi/teamb/Entities/patientTransportationRequest.java
@@ -1,3 +1,27 @@
 package edu.wpi.teamb.Entities;
 
-public class patientTransportationRequest extends generalRequest {}
+import lombok.Getter;
+import lombok.Setter;
+
+public class patientTransportationRequest extends generalRequest {
+    @Getter @Setter
+    private String equipmentNeeded;
+    @Getter @Setter
+    private String urgency;
+    @Getter @Setter
+    private String patientLocation;
+    @Getter @Setter
+    private String patientDestination;
+    @Getter @Setter
+    private String patientID;
+    private static patientTransportationRequest instance = null;
+
+
+    public static patientTransportationRequest getInstance() {
+        if (instance == null) {
+            instance = new patientTransportationRequest();
+        }
+        return instance;
+    }
+
+}

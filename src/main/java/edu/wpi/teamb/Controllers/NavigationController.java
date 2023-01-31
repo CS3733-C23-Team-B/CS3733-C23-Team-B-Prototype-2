03738382f708fbcd22f1115fc5403924d19cca06
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-01-31 14:46:04 -0500

    navname

--- src/main/java/edu/wpi/teamb/Controllers/NavigationController.java
+++ src/main/java/edu/wpi/teamb/Controllers/NavigationController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Controller {
+public class NavigationController {
   @FXML private Button homeButton;
   @FXML private BorderPane border;
 

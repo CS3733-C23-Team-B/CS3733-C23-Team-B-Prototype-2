d96438848f004b1d351a2b667ba701dde183326c
JackLafond <jacklafo12@gmail.com>
2023-02-04 19:29:54 -0500

    updates

--- src/main/java/edu/wpi/teamb/Database/DBSession.java
+++ src/main/java/edu/wpi/teamb/Database/DBSession.java
@@ -8,34 +8,35 @@ import org.hibernate.Transaction;
 
 public class DBSession {
 
-    private static DBSession instance = null;
+  private static DBSession instance = null;
 
-    private static HibernateUtil hu = new HibernateUtil();
+  private static HibernateUtil hu = new HibernateUtil();
 
-    private DBSession() {};
+  private DBSession() {};
 
-    public static DBSession getInstance() {
-        if(instance == null) {
-            instance = new DBSession();
-        }
-        return instance;
+  static {
+  }
+
+  public static DBSession getInstance() {
+    if (instance == null) {
+      instance = new DBSession();
     }
+    return instance;
+  }
 
-    public static void addTPRequest(PatientTransportationRequest ptr) {
-        SessionFactory sf = hu.getSessionFactory();
-        Session session = sf.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            session.persist(ptr);
-            tx.commit();
-        } catch (Exception e) {
-            if(tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+  public static void addTPRequest(PatientTransportationRequest ptr) {
+    SessionFactory sf = hu.getSessionFactory();
+    Session session = sf.openSession();
+    Transaction tx = null;
+    try {
+      tx = session.beginTransaction();
+      session.persist(ptr);
+      tx.commit();
+    } catch (Exception e) {
+      if (tx != null) tx.rollback();
+      e.printStackTrace();
+    } finally {
+      session.close();
     }
-
-
+  }
 }

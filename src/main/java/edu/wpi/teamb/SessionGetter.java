d9bc6335b32bb6d9dcc5e33b0ee2f170d58db872
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-02-05 14:34:09 -0500

    refactor

--- src/main/java/edu/wpi/teamb/SessionGetter.java
+++ src/main/java/edu/wpi/teamb/SessionGetter.java
@@ -21,7 +21,7 @@ public enum SessionGetter {
   private static SessionFactory buildSessionFactory() {
     try {
       // Create the SessionFactory from hibernate.cfg.xml
-      ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+      ssr = new StandardServiceRegistryBuilder().configure("edu/wpi/teamb/hibernate/hibernate.cfg.xml").build();
       System.out.println("Hibernate Configuration loaded");
 
       sessionFactory = new MetadataSources(ssr).buildMetadata().buildSessionFactory();

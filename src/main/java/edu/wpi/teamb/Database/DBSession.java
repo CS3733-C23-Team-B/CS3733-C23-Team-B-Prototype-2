d206ebd412544021edb15dc2d7747674f4da376f
JackLafond <jacklafo12@gmail.com>
2023-02-06 17:47:17 -0500

    updateNode fixes

--- src/main/java/edu/wpi/teamb/Database/DBSession.java
+++ src/main/java/edu/wpi/teamb/Database/DBSession.java
@@ -122,13 +122,13 @@ public class DBSession {
   }
 
   public static void updateLocationName(LocationName newLN, LocationName oldLN) {
-    if (newLN.getLongName() == oldLN.getLongName()) {
+    if (newLN.getLongName().equals(oldLN.getLongName())) {
       delete(oldLN);
       addORM(newLN);
     } else {
       List<Move> moves = getAllMoves();
       for (Move m : moves) {
-        if (m.getLongName() == oldLN.getLongName()) {
+        if (m.getLongName().equals(oldLN.getLongName())) {
           Move newm = new Move(m.getNodeID(), newLN.getLongName(), m.getMoveDate());
           addORM(newm);
           delete(m);
@@ -139,18 +139,32 @@ public class DBSession {
 
   public static void updateNode(Node n) {
 
-    Node ncopy = new Node("", n.getXcoord(), n.getYcoord(), n.getFloor(), n.getBuilding());
-    ncopy.setNodeID(ncopy.buildID());
-    System.out.println(ncopy.getNodeID());
+    Node ncopy = new Node();
+    ncopy.setNodeID(n.buildID());
+    ncopy.setXcoord(n.getXcoord());
+    ncopy.setYcoord(n.getYcoord());
+    ncopy.setFloor(n.getFloor());
+    ncopy.setBuilding(n.getBuilding());
 
     List<Edge> edges = getAllEdges();
     for (Edge e : edges) {
-      if (e.getNode1() == n.getNodeID()) {
-        Edge newe = new Edge(ncopy.getNodeID(), e.getNode2());
+      System.out.println(
+          "Edge node1: "
+              + e.getNode1()
+              + ", node2: "
+              + e.getNode2()
+              + ", Old NodeID: "
+              + n.getNodeID());
+      if (e.getNode1().equals(n.getNodeID())) {
+        Edge newe = new Edge();
+        newe.setNode1(ncopy.getNodeID());
+        newe.setNode2(e.getNode2());
         addORM(newe);
         delete(e);
-      } else if (e.getNode2() == n.getNodeID()) {
-        Edge newe = new Edge(e.getNode1(), ncopy.getNodeID());
+      } else if (e.getNode2().equals(n.getNodeID())) {
+        Edge newe = new Edge();
+        newe.setNode1(e.getNode1());
+        newe.setNode2(ncopy.getNodeID());
         addORM(newe);
         delete(e);
       }
@@ -158,8 +172,11 @@ public class DBSession {
 
     List<Move> moves = getAllMoves();
     for (Move m : moves) {
-      if (m.getNodeID() == n.getNodeID()) {
-        Move newm = new Move(ncopy.getNodeID(), m.getLongName(), m.getMoveDate());
+      if (m.getNodeID().equals(n.getNodeID())) {
+        Move newm = new Move();
+        newm.setNodeID(ncopy.getNodeID());
+        newm.setLongName(m.getLongName());
+        newm.setMoveDate(m.getMoveDate());
         addORM(newm);
         delete(m);
       }
@@ -178,71 +195,8 @@ public class DBSession {
     }
   }
 
-  public static void main(String[] args) throws ParseException {
+  public static void main(String[] args)  {
 
-    Node n = new Node();
-    n.setNodeID("test");
-    n.setYcoord(5);
-    n.setXcoord(5);
-    n.setFloor("L1");
-    n.setBuilding("testing");
-    Node n2 = new Node();
-    n2.setNodeID("test2");
-    n2.setXcoord(20);
-    n2.setYcoord(20);
-    n2.setFloor("L1");
-    n2.setBuilding("testing");
-    Node n3 = new Node();
-    n3.setNodeID("test3");
-    n3.setXcoord(25);
-    n3.setYcoord(25);
-    n3.setFloor("L1");
-    n3.setBuilding("testing");
-    //    addORM(n);
-    //    addORM(n2);
-    //    addORM(n3);
-    //
-    //    Edge e = new Edge();
-    //    e.setNode1(n.getNodeID());
-    //    e.setNode2(n2.getNodeID());
-    //    Edge e2 = new Edge();
-    //    e2.setNode1(n3.getNodeID());
-    //    e2.setNode2(n.getNodeID());
-    //    addORM(e);
-    //    addORM(e2);
-    //
-    //    LocationName ln = new LocationName();
-    //    ln.setLongName("test ln");
-    //    ln.setShortName("test sn");
-    //    ln.setLocationType("test lt");
-    //    LocationName ln2 = new LocationName();
-    //    ln2.setLongName("test ln2");
-    //    ln2.setShortName("test sn2");
-    //    ln2.setLocationType("test lt2");
-    //    LocationName ln3 = new LocationName();
-    //    ln3.setLongName("test ln3");
-    //    ln3.setShortName("test sn3");
-    //    ln3.setLocationType("test lt3");
-    //    addORM(ln);
-    //    addORM(ln2);
-    //    addORM(ln3);
-    //
-    //    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
-    //    Move m = new Move();
-    //    m.setNodeID(n.getNodeID());
-    //    m.setLongName(ln.getLongName());
-    //    m.setMoveDate(dateFormat.parse("01-01-2023"));
-    //    Move m2 = new Move();
-    //    m2.setNodeID(n2.getNodeID());
-    //    m2.setLongName(ln2.getLongName());
-    //    m2.setMoveDate(dateFormat.parse("01-01-2023"));
-    //    Move m3 = new Move();
-    //    m3.setNodeID(n3.getNodeID());
-    //    m3.setLongName(ln3.getLongName());
-    //    m3.setMoveDate(dateFormat.parse("01-01-2023"));
-    //    addORM(m);
-    //    addORM(m2);
-    //    addORM(m3);
-    updateNode(n);
+
   }
 }

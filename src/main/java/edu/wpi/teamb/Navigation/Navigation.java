89d6e1bb3d249d45510e91bca5a0458c0cb7cc19
Cierra <cierra362@gmail.com>
2023-02-12 18:20:17 -0500

    trying more things

--- src/main/java/edu/wpi/teamb/Navigation/Navigation.java
+++ src/main/java/edu/wpi/teamb/Navigation/Navigation.java
@@ -11,7 +11,7 @@ public class Navigation {
   public static void navigate(final Screen screen) {
     final String filename = screen.getFilename();
 
-    removeChild(1, 0);
+    // removeChild(1, 0);
     try {
       final var r = Bapp.class.getResource(filename);
       final FXMLLoader loader = new FXMLLoader(r);
@@ -28,7 +28,7 @@ public class Navigation {
         final FXMLLoader loader3 = new FXMLLoader(res);
 
         // Bapp.getRootPane().setConstraints(loader2.load(), 0, 0, 1, 1, HPos.CENTER, VPos.TOP);
-        Bapp.getRootPane().add(loader2.load(), 0, 0, 1, 1);
+        // Bapp.getRootPane().add(loader2.load(), 0, 0, 1, 1);
         Bapp.getRootPane().add(loader3.load(), 0, 2, 1, 1);
         // GridPane.setHalignment(loader.load(), HPos.RIGHT); this line as well as line 30 make all
         // other components afterwards not render

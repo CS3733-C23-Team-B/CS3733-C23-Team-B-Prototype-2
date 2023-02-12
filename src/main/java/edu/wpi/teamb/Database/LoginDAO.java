c45518817c0def31c75bdb82ba18e05c37016fe3
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-02-12 15:57:17 -0500

    Update LoginDAO.java

--- src/main/java/edu/wpi/teamb/Database/LoginDAO.java
+++ src/main/java/edu/wpi/teamb/Database/LoginDAO.java
@@ -1,11 +1,36 @@
 package edu.wpi.teamb.Database;
 
+import edu.wpi.teamb.Entities.SessionGetter;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LoginDAO {
     private static Map<String, Node> logins = new HashMap<String, Node>();
     public static Map<String, Node> getAllLogins() {
+        refreshLogins();
         return logins;
+    }
+
+    public static void refreshLogins() {
+        SessionFactory sf = SessionGetter.CONNECTION.getSessionFactory();
+        Session session = sf.openSession();
+        try {
+            Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("FROM Login ", Login.class);
+            List<Node> ns = q.list();
+            tx.commit();
+            logins.clear();
+            for (Node node : ns) logins.put(node.getNodeID(), node);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 }

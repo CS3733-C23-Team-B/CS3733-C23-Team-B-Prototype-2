b8cd33e1782d33100eb07b326db501d03509c34d
Jwalts23 <81712807+Jwalts23@users.noreply.github.com>
2023-02-12 17:07:27 -0500

    another one

--- src/main/java/edu/wpi/teamb/Controllers/ServiceRequest/BaseRequestController.java
+++ src/main/java/edu/wpi/teamb/Controllers/ServiceRequest/BaseRequestController.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -103,8 +105,8 @@ public class BaseRequestController {
   protected ObservableList<String> getLocations() {
     ObservableList<String> list = FXCollections.observableArrayList();
 
-    List<LocationName> locationNames = DBSession.getAllLocationNames();
-    locationNames.forEach(l -> list.add(l.getLongName()));
+    Map<String,LocationName> locationNames = DBSession.getAllLocationNames();
+    locationNames.forEach((key,value) -> list.add(value.getLongName()));
 
     Sorting.quickSort(list);
     return list;

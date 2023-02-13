0e696d5145efc47b54d7f3f1363e69033ef319bb
JackLafond <jacklafo12@gmail.com>
2023-02-13 15:29:48 -0500

    csv stuff

--- src/main/java/edu/wpi/teamb/Database/DatabaseWriteToCSV.java
+++ src/main/java/edu/wpi/teamb/Database/DatabaseWriteToCSV.java
@@ -2,7 +2,10 @@ package edu.wpi.teamb.Database;
 
 import edu.wpi.teamb.Database.DAO.MapDAO;
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Map;
 
 public class DatabaseWriteToCSV {
 
@@ -53,7 +56,60 @@ public class DatabaseWriteToCSV {
     }
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void writeNodes() throws IOException {
+    String fileName = "nodeWriteTo";
+    MapDAO.refreshNodes();
+    Map<String, Node> ns = MapDAO.getAllNodes();
+    boolean first = true;
+    for (Map.Entry<String, Node> en : ns.entrySet()) {
+      Node n = en.getValue();
+      String[] data = new String[5];
+      data[0] = n.getNodeID();
+      data[1] = Integer.toString(n.getXCoord());
+      data[2] = Integer.toString(n.getYCoord());
+      data[3] = n.getFloor();
+      data[4] = n.getBuilding();
+      writeCsv(fileName, data, first);
+      first = false;
+    }
+  }
+
+  public static void writeMoves() throws IOException, ParseException {
+    String fileName = "moveWriteTo";
+    Map<String, Move> ms =
+        MapDAO.getLNMoves(new SimpleDateFormat("yyyy-mm-dd").parse("3000-01-01"));
+    boolean first = true;
+    for (Map.Entry<String, Move> en : ms.entrySet()) {
+      Move m = en.getValue();
+      String[] data = new String[3];
+      data[0] = m.getNode().getNodeID();
+      data[1] = m.getLocationName().getLongName();
+      data[2] = m.getMoveDate().toString();
+      writeCsv(fileName, data, first);
+      first = false;
+    }
+  }
+
+  public static void writeLocationNames() throws IOException {
+    String fileName = "locationNameWriteTo";
+    MapDAO.refreshLocationNames();
+    Map<String, LocationName> lns = MapDAO.getAllLocationNames();
+    boolean first = true;
+    for (Map.Entry<String, LocationName> en : lns.entrySet()) {
+      LocationName ln = en.getValue();
+      String[] data = new String[3];
+      data[0] = ln.getLongName();
+      data[1] = ln.getShortName();
+      data[2] = ln.getLocationType();
+      writeCsv(fileName, data, first);
+      first = false;
+    }
+  }
+
+  public static void main(String[] args) throws IOException, ParseException {
     writeEdges();
+    writeNodes();
+    writeMoves();
+    writeLocationNames();
   }
 }

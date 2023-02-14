bdbbf49e13a9af2b0c4f75b5eed4b30ffed62aa5
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-02-14 13:18:31 -0500

    working date

--- src/main/java/edu/wpi/teamb/Controllers/Navigation/HomeController.java
+++ src/main/java/edu/wpi/teamb/Controllers/Navigation/HomeController.java
@@ -1,7 +1,8 @@
 package edu.wpi.teamb.Controllers.Navigation;
 
 import edu.wpi.teamb.Controllers.Profile.SigninController;
-import edu.wpi.teamb.Database.DAO.RequestDAO;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -9,10 +10,13 @@ import javafx.scene.layout.VBox;
 public class HomeController {
   @FXML Label homeWelcome;
   @FXML VBox homeRequests;
+  @FXML Label date;
 
   public void initialize() {
     homeWelcome.setText("Welcome, " + SigninController.currentUser.getFirstname());
-
-
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy");
+    String formattedDate = currentDate.format(formatter);
+    date.setText(formattedDate);
   }
 }

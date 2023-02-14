c75017b878158cb5b07c1570b24bdc457aa52537
lukegrady1 <lukegrady9@outlook.com>
2023-02-14 15:57:25 -0500

    update

--- src/main/java/edu/wpi/teamb/Controllers/Database/SideNodeEditor.java
+++ src/main/java/edu/wpi/teamb/Controllers/Database/SideNodeEditor.java
@@ -1,36 +1,35 @@
 package edu.wpi.teamb.Controllers.Database;
 
 import edu.wpi.teamb.Database.DBSession;
+import edu.wpi.teamb.Database.Move;
 import edu.wpi.teamb.Database.Node;
 import edu.wpi.teamb.Navigation.Navigation;
 import edu.wpi.teamb.Navigation.Screen;
 import edu.wpi.teamb.Pathfinding.Pathfinding;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
 import java.util.Collections;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class NodeEditorController {
-  @FXML Text nodeIDText;
+public class SideNodeEditor {
+
+  @FXML MFXFilterComboBox selectNodeID;
   @FXML TextField xField;
   @FXML TextField yField;
-  @FXML MFXFilterComboBox<String> floorBox;
   Node node = MapEditorController.getCurrentNode();
   String origFloor;
+  @FXML MFXButton edgesButton;
 
   public void initialize() {
     ObservableList<String> floors = FXCollections.observableArrayList();
     Collections.addAll(floors, "1", "2", "3", "L1", "L2");
     xField.setPromptText("" + node.getXCoord());
     yField.setPromptText("" + node.getYCoord());
-    origFloor = node.getFloor();
-    floorBox.setItems(floors);
-    floorBox.setValue(origFloor);
-    nodeIDText.setText(node.getNodeID());
+    selectNodeID.setItems(node.getNodeID( List< Move > moves = DBSession.getMostRecentMoves(node.getNodeID());));
   }
 
   public void submitClicked() {
@@ -38,7 +37,6 @@ public class NodeEditorController {
 
     String newX = xField.getText();
     String newY = yField.getText();
-    String newFloor = floorBox.getValue();
 
     if (!newX.isEmpty()) {
       node.setXCoord(Integer.parseInt(newX));
@@ -48,11 +46,6 @@ public class NodeEditorController {
       node.setYCoord(Integer.parseInt(newY));
       changed = true;
     }
-    if (!newFloor.equals(origFloor)) {
-      node.setFloor(newFloor);
-      changed = true;
-    }
-
     if (changed) {
       DBSession.updateNode(node);
       Node newNode = node;
@@ -69,11 +62,11 @@ public class NodeEditorController {
     MapEditorController.getInstance().refreshPopUp();
   }
 
-  public void edgesClicked() {
-    Navigation.navigate(Screen.EDGE_EDITOR);
-    Stage s = (Stage) yField.getScene().getWindow();
-    s.close();
-  }
+//  public void edgesClicked2() {
+//    Navigation.navigate(Screen.EDGE_EDITOR);
+//    Stage s = (Stage) yField.getScene().getWindow();
+//    s.close();
+//  }
 
   public void deleteClicked() {
     DBSession.deleteNode(node);

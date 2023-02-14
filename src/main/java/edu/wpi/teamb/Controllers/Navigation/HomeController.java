97f46a8e01f82b3247c9fd7f513dabfd523e5d68
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-02-14 14:19:51 -0500

    balls

--- src/main/java/edu/wpi/teamb/Controllers/Navigation/HomeController.java
+++ src/main/java/edu/wpi/teamb/Controllers/Navigation/HomeController.java
@@ -3,11 +3,15 @@ package edu.wpi.teamb.Controllers.Navigation;
 import edu.wpi.teamb.Controllers.Profile.SigninController;
 import edu.wpi.teamb.Database.DBSession;
 import edu.wpi.teamb.Database.GeneralRequest;
+import edu.wpi.teamb.Entities.RequestStatus;
+import edu.wpi.teamb.Navigation.Navigation;
+import edu.wpi.teamb.Navigation.Screen;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -33,15 +37,26 @@ public class HomeController {
         HBox box = new HBox();
         GeneralRequest r = l.get(i);
         Label label = new Label();
-        label.setText("Request #" + r.getId() + ", Status: " + r.getStatus());
+        label.setText("Request #" + r.getId() + ", Status: " + r.getStatus() + " ");
         label.setPadding(new Insets(15, 0, 15, 20));
         label.setFont(new Font("Nunito", 20));
         box.getChildren().add(label);
+        box.setAlignment(Pos.CENTER_LEFT);
         ImageView im = new ImageView();
-        im.setImage(new Image("/media/status/image 3.png"));
+        im.setFitHeight(20);
+        im.setFitWidth(20);
+        if (r.getStatus() == RequestStatus.PROCESSING) {
+          im.setImage(new Image("/media/status/clock.png"));
+        } else if (r.getStatus() == RequestStatus.DONE) {
+          im.setImage(new Image("/media/status/done.png"));
+        }
         box.getChildren().add(im);
         homeRequests.getChildren().add(box);
       }
     }
+  }
+
+  public void allRequests() {
+    Navigation.navigate(Screen.REQUESTS);
   }
 }

31537c6e91cda7a2eff1138dd0408708f4ee913f
kavyamani02 <kavya.t.mani@gmail.com>
2023-02-21 17:26:38 -0500

    changes

---
+++ src/main/java/edu/wpi/teamb/Controllers/Navigation/ForgotPassController 2.java
@@ -0,0 +1,44 @@
+package edu.wpi.teamb.Controllers.Navigation;
+
+import static edu.wpi.teamb.Database.SendEmail.sendEmail;
+
+import edu.wpi.teamb.Database.DBSession;
+import edu.wpi.teamb.Database.Login;
+import edu.wpi.teamb.Navigation.Popup;
+import edu.wpi.teamb.Navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.util.Map;
+import javafx.fxml.FXML;
+import javax.mail.MessagingException;
+
+public class ForgotPassController {
+  @FXML MFXTextField username;
+  @FXML MFXButton sendPassword;
+
+  public void sendPassword() {
+    Map<String, Login> loginMap = DBSession.getAllLogins();
+    if (username.getText().length() > 0) {
+      try {
+        String user = "bodaciousbadger1@gmail.com";
+        String password = "ftannejwvxyokvet";
+        String recipient = loginMap.get(username.getText()).getEmail();
+        String subject = "Forgot Password";
+        String message =
+            "Dear "
+                + loginMap.get(username.getText()).getFirstname()
+                + ",\nYour Password is: "
+                + loginMap.get(username.getText()).getPassword();
+        sendEmail(user, password, recipient, subject, message);
+        System.out.println("Email sent successfully.");
+      } catch (MessagingException ex) {
+        System.out.println("Failed to send email: " + ex.getMessage());
+      }
+      Popup.hidePopup(Screen.FORGOT_PASSWORD);
+    }
+  }
+
+  public void close() {
+    Popup.hidePopup(Screen.FORGOT_PASSWORD);
+  }
+}
---
+++ src/main/java/edu/wpi/teamb/Controllers/Profile/CreateAccountController 2.java
@@ -0,0 +1,80 @@
+package edu.wpi.teamb.Controllers.Profile;
+
+import edu.wpi.teamb.Database.DBSession;
+import edu.wpi.teamb.Database.Login;
+import edu.wpi.teamb.Navigation.Popup;
+import edu.wpi.teamb.Navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+public class CreateAccountController {
+
+  @FXML MFXTextField firstNameField;
+  @FXML MFXTextField lastNameField;
+  @FXML MFXTextField emailField;
+  @FXML MFXTextField usernameField;
+  @FXML MFXTextField passwordField;
+  @FXML MFXButton backButton;
+  @FXML Label notificationText;
+
+  private Map<String, Login> usersMap = new HashMap<>();
+  private List<Login> users = new ArrayList<Login>();
+
+  public void backButtonClicked() {
+    Popup.hidePopup(Screen.CREATE_ACCOUNT);
+  }
+
+  public void submitClicked(ActionEvent actionEvent) {
+    if (!firstNameField.getText().equals("")
+        && !lastNameField.getText().equals("")
+        && !emailField.getText().equals("")
+        && !usernameField.getText().equals("")
+        && !passwordField.getText().equals("")) {
+      usersMap = DBSession.getAllLogins();
+      usersMap.forEach(
+          (key, value) -> {
+            users.add(value);
+          });
+      boolean exists = false;
+      // check if username already exists
+      for (Object user : users) {
+        Login u = (Login) user;
+        if (u.getUsername().equals(usernameField.getText())) {
+          notificationText.setTextFill(Paint.valueOf("RED"));
+          notificationText.setText("account with that username already exists");
+          exists = true;
+          break;
+        }
+      }
+      if (!exists) {
+        notificationText.setTextFill(Color.GREEN);
+        notificationText.setText("creating new login");
+        Login newLogin =
+            new Login(
+                usernameField.getText(),
+                passwordField.getText(),
+                emailField.getText(),
+                firstNameField.getText(),
+                lastNameField.getText());
+        users.add(newLogin);
+        DBSession.addLogin(newLogin);
+        Stage s = (Stage) backButton.getScene().getWindow();
+        Popup.hidePopup(Screen.CREATE_ACCOUNT);
+        SigninController.getInstance().refresh();
+      }
+    } else {
+      notificationText.setTextFill(Color.RED);
+      notificationText.setText("Please fill in all fields");
+    }
+  }
+}

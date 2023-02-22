56a876450e7dc14c9643678a9e659d924345da10
swlendrum <slendrum465@gmail.com>
2023-02-21 23:52:46 -0500

    Dragging node brings location name

--- src/main/java/edu/wpi/teamb/Controllers/Database/MapEditorController.java
+++ src/main/java/edu/wpi/teamb/Controllers/Database/MapEditorController.java
@@ -76,6 +76,7 @@ public class MapEditorController {
   @FXML MFXFilterComboBox<String> floorCombo;
   private Map<String, String> floorMap = new HashMap<>();
   private Map<String, ImageView> imageMap = new HashMap<>();
+  private Map<Node, AnchorPane> locationMap = new HashMap<>();
   private String currentFloor;
   @FXML VBox mapEditorButtons;
 
@@ -336,6 +337,8 @@ public class MapEditorController {
     popPane.setTranslateX(dot.getCenterX() + dot.getRadius() * 2 - 50);
     popPane.setTranslateY(dot.getCenterY() - dot.getRadius() * 2 + 38);
 
+    locationMap.put(node, popPane);
+
     VBox vbox = new VBox();
     popPane.getChildren().add(vbox);
     List<Move> l = moveMap.get(node.getNodeID());
@@ -414,7 +417,14 @@ public class MapEditorController {
     dot.setOnMouseReleased(
         e -> {
           pane.setGestureEnabled(true);
-          if (dragged) updateNode(dot);
+          if (dragged) {
+            updateNode(dot);
+            AnchorPane popPane = locationMap.get(nodeMap.get(dot));
+            if (popPane != null) {
+              popPane.setTranslateX(dot.getCenterX() + dot.getRadius() * 2 - 50);
+              popPane.setTranslateY(dot.getCenterY() - dot.getRadius() * 2 + 38);
+            }
+          }
           dragged = false;
         });
 

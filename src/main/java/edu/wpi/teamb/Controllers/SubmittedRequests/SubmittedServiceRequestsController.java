1837e3e03cbcba219f278900025f97a08109d56c
Jwalts23 <81712807+Jwalts23@users.noreply.github.com>
2023-02-23 12:15:54 -0500

    working on fixing formatting

--- src/main/java/edu/wpi/teamb/Controllers/SubmittedRequests/SubmittedServiceRequestsController.java
+++ src/main/java/edu/wpi/teamb/Controllers/SubmittedRequests/SubmittedServiceRequestsController.java
@@ -12,6 +12,8 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -21,11 +23,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 
 public class SubmittedServiceRequestsController {
   @FXML VBox mainVbox;
+  @FXML Label RequestInformationTitle;
   @FXML VBox specificRequestInfoBox;
+  @FXML VBox generalRequestInfoVbox;
   @FXML VBox filterVbox;
   @FXML MFXButton clearFiltersButton;
   @FXML MFXComboBox requestStatusFilter;
@@ -40,6 +43,13 @@ public class SubmittedServiceRequestsController {
   SubmittedAVRequestTable avTable = new SubmittedAVRequestTable();
   SubmittedSecurityRequestTable securityTable = new SubmittedSecurityRequestTable();
   SubmittedGeneralRequestTable allTable = new SubmittedGeneralRequestTable();
+
+  List<String> titles = new ArrayList<>();
+  List<String> data = new ArrayList<>();
+  @FXML Label field1label, field2label, field3label, field4label;
+  List<Label> Labels = new ArrayList<>();
+  @FXML Label field1text, field2text, field3text, field4text;
+  List<Label> text = new ArrayList<>();
 
   @FXML Label la;
 
@@ -97,9 +107,24 @@ public class SubmittedServiceRequestsController {
     requestStatusFilter.setText("--Select--");
     assignedStaffFilter.setText("--Select--");
     requestUrgencyFilter.setText("--Select--");
+    resetRequestVboxes();
+  }
+
+  public void initLabels() {
+    Labels.add(field1label);
+    Labels.add(field2label);
+    Labels.add(field3label);
+    Labels.add(field4label);
+    text.add(field1text);
+    text.add(field2text);
+    text.add(field3text);
+    text.add(field4text);
+  }
+
+  public void resetRequestVboxes() {
     specificRequestInfoBox.getChildren().clear();
-    RequestInformationTitle.setText("Request Info");
-    specificRequestInfoBox.getChildren().add(RequestInformationTitle);
+    generalRequestInfoVbox.getChildren().clear();
+    generalRequestInfoVbox.getChildren().add(RequestInformationTitle);
   }
 
   public void helpButtonClicked() throws IOException {
@@ -177,81 +202,77 @@ public class SubmittedServiceRequestsController {
     filter();
   }
 
-  @FXML Label RequestInformationTitle;
-
   @FXML
   public void mouseClicked(TableView table) {
     GeneralRequest r = (GeneralRequest) table.getSelectionModel().getSelectedItem();
-    specificRequestInfoBox.getChildren().clear();
-    RequestInformationTitle.setText("Request Info");
-    specificRequestInfoBox.getChildren().add(RequestInformationTitle);
+    resetRequestVboxes();
+    titles = new ArrayList<>();
+    data = new ArrayList<>();
 
     if (r != null) {
       addCommonAttritbutes(r.getRequestType().toString(), r.getDate(), r.getUrgency().toString());
       if (r.getRequestType().equals(RequestType.PATIENTTRANSPOTATION)) {
         PatientTransportationRequest pt = (PatientTransportationRequest) r;
-        Label patientDestination =
-            addAttribute("Patient Destination:", pt.getPatientDestinationLocation());
-        Label patientID = addAttribute("Patient ID:", pt.getPatientID());
-        Label patientCurrent =
-            addAttribute("Patient Current Location:", pt.getPatientCurrentLocation());
-        Label equipmentNeeded = addAttribute("Equipment Needed:", pt.getEquipmentNeeded());
-
+        addAttribute("Patient Destination:", pt.getPatientDestinationLocation());
+        addAttribute("Patient ID:", pt.getPatientID());
+        addAttribute("Patient Current Location:", pt.getPatientCurrentLocation());
+        addAttribute("Equipment Needed:", pt.getEquipmentNeeded());
+        setFields();
       } else if (r.getRequestType().equals(RequestType.SANITATION)) {
         SanitationRequest sr = (SanitationRequest) r;
-        Label cleanUpLocation = addAttribute("Clean Up Location:", sr.getCleanUpLocation());
-        Label typeOfCleanUp = addAttribute("Type of Clean Up:", sr.getTypeOfCleanUp());
+        addAttribute("Clean Up Location:", sr.getCleanUpLocation());
+        addAttribute("Type of Clean Up:", sr.getTypeOfCleanUp());
+        setFields();
       } else if (r.getRequestType().equals(RequestType.COMPUTER)) {
         ComputerRequest cr = (ComputerRequest) r;
-        Label typeOfRepair = addAttribute("Type of Repair:", cr.getTypeOfRepair());
-        Label device = addAttribute("Type of Device:", cr.getDevice());
-        Label repairLocation = addAttribute("Repair Location:", cr.getRepairLocation());
+        addAttribute("Type of Repair:", cr.getTypeOfRepair());
+        addAttribute("Type of Device:", cr.getDevice());
+        addAttribute("Repair Location:", cr.getRepairLocation());
+        setFields();
       } else if (r.getRequestType().equals(RequestType.AUDOVISUAL)) {
         AudioVideoRequest avr = (AudioVideoRequest) r;
-        Label avType = addAttribute("Audio Visual Type:", avr.getAVType());
-        Label location = addAttribute("Location:", avr.getLocation());
+        addAttribute("Audio Visual Type:", avr.getAVType());
+        addAttribute("Location:", avr.getLocation());
+        setFields();
       } else if (r.getRequestType().equals(RequestType.SECURITY)) {
         SecurityRequest secr = (SecurityRequest) r;
-        Label issueType = addAttribute("Type Of Issue:", secr.getIssueType());
-        Label equipmentNeeded = addAttribute("Equipment Needed:", secr.getEquipmentNeeded());
-        Label NumberRequired =
-            addAttribute("Number Required:", String.valueOf(secr.getNumberRequired()));
+        addAttribute("Type Of Issue:", secr.getIssueType());
+        addAttribute("Equipment Needed:", secr.getEquipmentNeeded());
+        addAttribute("Number Required:", String.valueOf(secr.getNumberRequired()));
+        setFields();
       }
     } else {
 
     }
   }
 
-  private Label addAttribute(String title, String field) {
-    Label t = new Label();
-    int fontSize = 16;
-    String font = "Nunito";
-    Label l = new Label();
-    Font f = new Font(font, fontSize);
-    l.setFont(Font.font(font, fontSize));
-    l.setText(field);
-    t.setFont(Font.font(font, FontWeight.LIGHT, fontSize));
-    t.setText(title);
-
-    specificRequestInfoBox.getChildren().add(t);
-    specificRequestInfoBox.getChildren().add(l);
-    return l;
+  private void addAttribute(String title, String field) {
+    titles.add(title);
+    data.add(field);
   }
 
-  private Label addCommonAttritbute(String field) {
-    Label t = new Label();
-    int fontSize = 22;
-    String font = "Nunito";
-    t.setFont(Font.font(font, fontSize));
-    t.setText(field);
-    specificRequestInfoBox.getChildren().add(t);
-    return t;
-  }
+  @FXML Label requestTypeText;
+  @FXML Label dateText;
+  @FXML Label urgencyText;
+  @FXML Label UrgencyLabel;
 
   private void addCommonAttritbutes(String type, String date, String urgency) {
-    addCommonAttritbute(type);
-    addCommonAttritbute(date);
-    addAttribute("Request Urgency:", urgency);
+    requestTypeText.setText(type);
+    generalRequestInfoVbox.getChildren().add(requestTypeText);
+    dateText.setText(date);
+    generalRequestInfoVbox.getChildren().add(dateText);
+    urgencyText.setText(urgency);
+    specificRequestInfoBox.getChildren().add(UrgencyLabel);
+    specificRequestInfoBox.getChildren().add(urgencyText);
+  }
+
+  private void setFields() {
+    for (int i = 0; i < data.size(); i++) {
+      Labels.get(i).setText(titles.get(i));
+      text.get(i).setText(data.get(i));
+      specificRequestInfoBox.getChildren().add(Labels.get(i));
+      specificRequestInfoBox.getChildren().add(text.get(i));
+    }
   }
 
   private void setFilters() {

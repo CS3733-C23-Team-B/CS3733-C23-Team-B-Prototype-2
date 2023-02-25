0f7c36b573724271c87569e85445ec0b537e492a
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-02-25 16:04:53 -0500

    added date

--- src/main/java/edu/wpi/teamb/Controllers/Database/KioskEditController.java
+++ src/main/java/edu/wpi/teamb/Controllers/Database/KioskEditController.java
@@ -2,17 +2,26 @@ package edu.wpi.teamb.Controllers.Database;
 
 import edu.wpi.teamb.Database.DBSession;
 import edu.wpi.teamb.Database.Move;
-import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class KioskEditController {
 
-  @FXML MFXComboBox moveDropdown;
+  @FXML MFXFilterComboBox moveDropdown;
   @FXML MFXTextField moveMessage;
+  @FXML Label timeLabel;
+  @FXML Label dateLabel;
 
   public void initialize() {
     List<Move> moves = DBSession.getAllMoves();
@@ -21,6 +30,24 @@ public class KioskEditController {
             .map(move -> move.getLocationName().getLongName())
             .collect(Collectors.toList());
     moveDropdown.setItems(FXCollections.observableList(l));
+
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy");
+    String formattedDate = currentDate.format(formatter);
+
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(0),
+                event -> {
+                  LocalDateTime currentTime = LocalDateTime.now();
+                  DateTimeFormatter timefmt = DateTimeFormatter.ofPattern("h:mm a");
+                  timeLabel.setText(currentTime.format(timefmt));
+                }),
+            new KeyFrame(Duration.seconds(1)));
+    timeline.setCycleCount(Timeline.INDEFINITE);
+    timeline.play();
+    dateLabel.setText(formattedDate);
   }
 
   public void addKiosk() {

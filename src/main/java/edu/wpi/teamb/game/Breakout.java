974fadbd1903bdfe91bf3d7ba811bdcdaad6a950
MegaGamer-Git <magatti2@gmail.com>
2023-02-28 23:05:29 -0500

    Working ball game

--- src/main/java/edu/wpi/teamb/game/Breakout.java
+++ src/main/java/edu/wpi/teamb/game/Breakout.java
@@ -1,3 +1,33 @@
 package edu.wpi.teamb.game;
 
-public class Breakout {}
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Breakout extends JFrame {
+
+  public Breakout() {
+
+    initUI();
+  }
+
+  private void initUI() {
+
+    add(new Board());
+    setTitle("Badger Breaker");
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setResizable(false);
+    pack();
+  }
+
+  public static void startGame(){
+
+    EventQueue.invokeLater(
+        () -> {
+          var game = new Breakout();
+          game.setLocationRelativeTo(null);
+          game.setVisible(true);
+        });
+  }
+}
